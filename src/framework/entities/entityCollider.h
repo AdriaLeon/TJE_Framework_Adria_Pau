0b78535b02eca4a83191a5f7191dd2b6092ba533
AdriaLeon <adria.leon01@estudiant.upf.edu>
2024-05-17 11:04:01 +0200

    ..

--- src/framework/entities/entityCollider.h
+++ src/framework/entities/entityCollider.h
@@ -1,1 +1,19 @@
+#include "entityMesh.h"
 
+enum {
+	NONE = 0,
+	FLOOR = 1 << 0,
+	WALL = 1 << 1,
+	PLAYER = 1 << 2,
+	// Both WALL and FLOOR are considered SCENARIO
+	// using OR operator
+	SCENARIO = WALL | FLOOR,
+	ALL = 0xFF
+};
+
+class EntityCollider : public EntityMesh {
+public:
+	bool is_dynamic = false;
+	int layer = NONE;
+
+};
